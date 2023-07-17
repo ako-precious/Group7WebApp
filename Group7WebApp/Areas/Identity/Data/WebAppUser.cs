16ab780dd256ad9461b94b4f74907b335198a638
AKO PRECIOUS <akoprecious58@gmail.com>
2023-07-17 11:33:21 -0400

    Add project files.

---
+++ Group7WebApp/Areas/Identity/Data/WebAppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Group7WebApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the WebAppUser class
+public class WebAppUser : IdentityUser
+{
+    [ProtectedPersonalData]
+    [Required]
+    public string FirtName { get; set; }
+    [ProtectedPersonalData]
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public string Role { get; set; }
+}
+
