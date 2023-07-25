495a5c581f7eeaa378a185afb10b0b280f0bb29e
AKO PRECIOUS <akoprecious58@gmail.com>
2023-07-25 11:01:00 -0400

    We are making lot of progress here😘😍

--- Group7WebApp/Controllers/UserController.cs
+++ Group7WebApp/Controllers/UserController.cs
@@ -10,15 +10,73 @@ namespace Group7WebApp.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<WebAppUser> _userManager;
+        private readonly SignInManager<WebAppUser> _signInManager;
+
         private readonly AuthDbContext _context;
-        public UserController(AuthDbContext context, UserManager<WebAppUser> userManager)
+        public UserController(AuthDbContext context, UserManager<WebAppUser> userManager, SignInManager<WebAppUser> signInManager)
         {
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         public IActionResult Index()
         {
+
+            return View(_userManager.Users.ToList());
+        }
+
+        public IActionResult Details(string id)
+        {
+            var user = _userManager.Users.FirstOrDefault(p => p.Id == id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
             return View();
         }
+        public IActionResult Delete(string id)
+        {
+            var User = _userManager.Users.FirstOrDefault(p => p.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return View(User);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            // Find the user by ID
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                // Handle the case where the user is not found
+                return NotFound();
+            }
+
+            // Delete the user
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                // If the user was successfully deleted, you can perform additional actions if needed.
+                // For example, you can sign out the user (optional).
+                await _signInManager.SignOutAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // If there are errors during the deletion process, handle them appropriately.
+                // For example, you can display an error message to the user.
+                ModelState.AddModelError("", "User deletion failed. Please try again.");
+                return View("DeleteUserConfirmation");
+            }
+        }
+
     }
 }
---
+++ Group7WebApp/Controllers/UsersRoleController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Group7WebApp.Areas.Identity.Data;
+using Group7WebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+using Group7WebApp.Models;
+using System.Data;
+
+namespace Group7WebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersRole : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<WebAppUser> _userManager;
+
+
+        public UsersRole( RoleManager<IdentityRole> roleManager, UserManager<WebAppUser> userManager)
+        {            
+            _roleManager = roleManager;
+            _userManager = userManager;
+           
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+        public IActionResult Details(string id)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(p => p.Id == id);
+
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+            var usersInRole = _userManager.GetUsersInRoleAsync(role.Name).Result;
+
+            ViewBag.RoleName = role.Name;
+            ViewBag.UsersInRole = usersInRole;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        } 
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+
+            if (ModelState.IsValid & !_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+                
+                TempData["success"] = "Role created successfully";
+            }
+                return RedirectToAction(nameof(Index));
+            
+        }
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(p => p.Id == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(IdentityRole model )
+        {
+            var existingRole = await _roleManager.FindByIdAsync(model.Id);
+
+            if (existingRole == null)
+            {
+                // Handle role not found error
+                return NotFound();
+            }
+            // Update the role properties
+            existingRole.Name = model.Name;
+
+            // Save the changes
+            var result = await _roleManager.UpdateAsync(existingRole);
+
+            if (result.Succeeded)
+            {
+                // Role updated successfully
+                TempData["success"] = "Role Edited successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(p => p.Id == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(IdentityRole model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role != null)
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    // Role deleted successfully
+                    TempData["success"] = "Role Deleted successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            else
+            {
+                // Role not found
+                return NotFound();
+            }
+
+            }
+
+    }
+
+}
