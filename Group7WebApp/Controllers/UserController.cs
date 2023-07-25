2f5bc108a74fc63e12419bc8e4f0add0601faaa7
AKO PRECIOUS <akoprecious58@gmail.com>
2023-07-25 19:02:54 -0400

    User Information updated

--- Group7WebApp/Controllers/UserController.cs
+++ Group7WebApp/Controllers/UserController.cs
@@ -1,12 +1,15 @@
 ﻿using Group7WebApp.Areas.Identity.Data;
 using Group7WebApp.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Polly;
+using System.Data;
 
 namespace Group7WebApp.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
         private readonly UserManager<WebAppUser> _userManager;
@@ -33,24 +36,24 @@ namespace Group7WebApp.Controllers
             {
                 return NotFound();
             }
-            return View();
+            return View(user);
         }
         public IActionResult Delete(string id)
         {
-            var User = _userManager.Users.FirstOrDefault(p => p.Id == id);
+            var user = _userManager.Users.FirstOrDefault(p => p.Id == id);
 
-            if (User == null)
+            if (user == null)
             {
                 return NotFound();
             }
-            return View(User);
+            return View(user);
         }
 
         [HttpPost]
-        public async Task<IActionResult> DeleteUser(string userId)
+        public async Task<IActionResult> Delete(WebAppUser userr)
         {
             // Find the user by ID
-            var user = await _userManager.FindByIdAsync(userId);
+            var user = await _userManager.FindByIdAsync(userr.Id);
 
             if (user == null)
             {
@@ -66,15 +69,15 @@ namespace Group7WebApp.Controllers
                 // If the user was successfully deleted, you can perform additional actions if needed.
                 // For example, you can sign out the user (optional).
                 await _signInManager.SignOutAsync();
-
-                return RedirectToAction("Index", "Home");
+                TempData["success"] = "User Account Deleted successfully";
+                return RedirectToAction(nameof(Index));
             }
             else
             {
                 // If there are errors during the deletion process, handle them appropriately.
                 // For example, you can display an error message to the user.
                 ModelState.AddModelError("", "User deletion failed. Please try again.");
-                return View("DeleteUserConfirmation");
+                return View();
             }
         }
 
